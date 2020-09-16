0f847af795a85db4447d493ee92ac9d91550fe73
gungoren <mehmetgungoren@lyrebirdstudio.net>
2020-09-16 14:25:36 +0300

    handle case sensitive as well.

--- telegramRSSbot.py
+++ telegramRSSbot.py
@@ -213,7 +213,7 @@ def send_message_to_chat(context, rss_entry):
     else:
         send_message = check_entry_budget(detail)
 
-    if send_message and check_entry_contains_banned_word(detail):
+    if send_message and check_entry_contains_banned_word(str(detail).lower()):
         context.bot.send_message(chatid, rss_entry['link'])
 
 
